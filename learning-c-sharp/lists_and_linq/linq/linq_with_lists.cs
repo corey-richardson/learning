fcc67cf73ff1177526f8da85f0d2c5728db5494d
corey-richardson <richardsoncorey04@gmail.com>
2023-01-31 09:19:55 +0000

    LINQ with Other Collections

---
+++ learning-c-sharp/lists_and_linq/linq/linq_with_lists.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLinq
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<string> heroesList = new List<string> 
+      { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+      // Query heroesList to find all hero names that contain either a '.'
+      // or '7' (a period or the number 7).
+      var containsPeriodOrSeven = heroesList
+      .Where(h => h.Contains(".") || h.Contains("7"));
+
+      foreach(string h in containsPeriodOrSeven)
+      { Console.WriteLine(h); }
+  
+    }
+  }
+}
